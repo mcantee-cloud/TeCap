766c55d14c8d5d4b8c35c158bff45957d3135861
mcantee-cloud <mcantee@miumg.edu.gt>
2025-09-30 20:52:39 -0600

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+function verMas(seccion) {
+  if (seccion === "tequila") {
+    alert("Tequila Sounds: Renta de audio, eventos y artistas disponibles. 🎶🍹");
+  } else if (seccion === "capturando") {
+    alert("Capturando el Momento: Viajes y experiencias únicas. 🌍📸");
+  }
+}
